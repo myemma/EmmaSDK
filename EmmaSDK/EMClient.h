7223597e352ac41a5350c87c013fd3ae2274b7f0
Benjamin van der Veen <b@bvanderveen.com>
2013-03-18 20:12:11 -0700

    headers for members, searches

--- EmmaSDK/EMClient.h
+++ EmmaSDK/EMClient.h
@@ -16,6 +16,8 @@ typedef struct EMResultRange EMResultRange;
 
 @protocol EMClient <NSObject>
 
+// groups
+
 - (RACSignal *)getGroupCountWithType:(EMGroupType)groupType;
 - (RACSignal *)getGroupsWithType:(EMGroupType)groupType inRange:(EMResultRange)range; // returns NSArray of EMGroup
 
@@ -24,6 +26,11 @@ typedef struct EMResultRange EMResultRange;
 - (RACSignal *)deleteGroupID:(NSString *)groupID;
 - (RACSignal *)addMemberIDs:(NSArray *)memberIDs toGroupID:(NSString *)groupID;
 - (RACSignal *)removeMemberIDs:(NSArray *)memberIDs fromGroupID:(NSString *)groupID;
+
+- (RACSignal *)getMemberCountInGroupID:(NSString *)groupID includeDeleted:(BOOL)deleted; // returns NSNumber
+- (RACSignal *)getMembersInGroupID:(NSString *)groupID inRange:(EMResultRange)range includeDeleted:(BOOL)deleted; // returns NSArray of EMMember
+
+// mailings
 
 - (RACSignal *)getMailingWithID:(NSString *)mailingID;
 
@@ -49,26 +56,49 @@ typedef struct EMResultRange EMResultRange;
 - (RACSignal *)validateMailingID:(NSString *)mailingID; // returns @YES if mailing is valid, otherwise result should contain info about errors
 - (RACSignal *)declareWinnerID:(NSString *)winner forMailingID:(NSString *)mailingID;
 
+// members
+
 - (RACSignal *)getMemberCount; // returns NSNumber
 - (RACSignal *)getMembersInRange:(EMResultRange)range; // returns NSArray of EMMember
 
-- (RACSignal *)getMemberCountInGroupID:(NSString *)groupID includeDeleted:(BOOL)deleted; // returns NSNumber
-- (RACSignal *)getMembersInGroupID:(NSString *)groupID inRange:(EMResultRange)range includeDeleted:(BOOL)deleted; // returns NSArray of EMMember
+- (RACSignal *)getMemberWithID:(NSString *)memberID;
+- (RACSignal *)getMemberWithEmail:(NSString *)email;
+- (RACSignal *)getOptoutInfoForMemberID:(NSString *)memberID; // XXX response format undefined
+- (RACSignal *)optoutMemberWithEmail:(NSString *)email;
+- (RACSignal *)createMembers:(NSArray *)members withSourceName:(NSString *)sourceName addOnly:(BOOL)addOnly groupIDs:(NSArray *)groupIDs;
+- (RACSignal *)createMember:(EMMember *)member;
+- (RACSignal *)deleteMembersWithIDs:(NSArray *)memberIDs;
+- (RACSignal *)updateMemberIDs:(NSArray *)memberIDs withStatus:(EMMemberStatus)status;
+- (RACSignal *)updateMember:(EMMember *)member;
+// - (RACSignal *)deleteMemberWithID:(NSString *)memberID // redundant, skip
+- (RACSignal *)getGroupsForMemberID:(NSString *)memberID;
+ - (RACSignal *)addMemberID:(NSString *)memberID toGroupIDs:(NSArray *)groupIDs;
+//- (RACSignal *)removeMemberID:(NSString *)memberID fromGroupIDs:(NSArray *)groupIDs; // redundant, skip
+- (RACSignal *)deleteMembersWithStatus:(EMMemberStatus)status;
+- (RACSignal *)removeMemberFromAllGroups:(NSString *)member;
+- (RACSignal *)removeMemberIDs:(NSArray *)memberIDs fromGroupIDs:(NSArray *)groupIDs;
+- (RACSignal *)getMailingHistoryForMemberID:(NSString *)memberID;
+- (RACSignal *)getMembersForImportID:(NSString *)importID;
+- (RACSignal *)getImportID:(NSString *)importID;
+- (RACSignal *)getImports;
+//- (RACSignal *)deleteImport;
+- (RACSignal *)copyMembersWithStatuses:(EMMemberStatus)status toGroup:(NSString *)groupIDs;
+- (RACSignal *)updateMembersWithStatus:(EMMemberStatus)fromStatus toStatus:(EMMemberStatus)toStatus limitByGroupID:(NSString *)groupID;
 
+// searches
+
+- (RACSignal *)getSearchCount; // returns NSArray of EMSearch
+- (RACSignal *)getSearchesInRange:(EMResultRange)range; // returns NSArray of EMSearch
+- (RACSignal *)getSearchID:(NSString *)searchID;
+- (RACSignal *)createSearch:(EMSearch *)search;
+- (RACSignal *)updateSearch:(EMSearch *)search;
+- (RACSignal *)deleteSearchID:(NSString *)searchID;
 - (RACSignal *)getMemberCountInSearchID:(NSString *)searchID; // returns NSNumber
 - (RACSignal *)getMembersInSearchID:(NSString *)searchID inRange:(EMResultRange)range; // returns NSArray of EMMember
-
-- (RACSignal *)getMemberWithID:(NSString *)memberID;
-- (RACSignal *)createMember:(EMMember *)member;
-- (RACSignal *)updateMember:(EMMember *)member;
-- (RACSignal *)deleteMembersWithIDs:(NSArray *)memberIDs;
-- (RACSignal *)addMemberID:(NSString *)memberID toGroupIDs:(NSArray *)groupIDs;
-- (RACSignal *)removeMemberID:(NSString *)memberID fromGroupIDs:(NSArray *)groupIDs;
 
 - (RACSignal *)getMessageCountForMemberID:(NSString *)memberID; // returns NSNumber
 - (RACSignal *)getMessagesForMemberID:(NSString *)memberID inRange:(EMResultRange)range; // returns NSArray of EMMessage
 
-- (RACSignal *)getSearchesInRange:(EMResultRange)range; // returns NSArray of EMSearch
 
 - (RACSignal *)getResponseForMailingID:(NSString *)mailingID; // returns EMMailingResponse
 - (RACSignal *)getLinkResponseForMailingID:(NSString *)mailingID; // returns EMMailingLinkResponse
