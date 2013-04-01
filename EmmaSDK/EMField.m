c207c498f014b433ed70818739b549b58e5dab0b
Benjamin van der Veen <b@bvanderveen.com>
2013-04-01 14:09:41 -0700

    de-dupe field type parsing

--- EmmaSDK/EMField.m
+++ EmmaSDK/EMField.m
@@ -88,39 +88,11 @@ NSString *EMFieldWidgetTypeToString(EMFieldWidgetType type) {
         
         NSString *fieldTypeString = [[dict objectForKey:@"field_type"] stringOrNil];
         
-        if ([fieldTypeString isEqual:@"text"])
-            _fieldType = EMFieldTypeText;
-        else if ([fieldTypeString isEqual:@"text[]"])
-            _fieldType = EMFieldTypeTextArray;
-        else if ([fieldTypeString isEqual:@"date"])
-            _fieldType = EMFieldTypeDate;
-        else if ([fieldTypeString isEqual:@"timestamp"])
-            _fieldType = EMFieldTypeTimestamp;
-        else if ([fieldTypeString isEqual:@"numeric"])
-            _fieldType = EMFieldTypeNumeric;
-        else if ([fieldTypeString isEqual:@"boolean"])
-            _fieldType = EMFieldTypeBoolean;
-        else
-            NSLog(@"-[Field initWithDictionary]: encountered unknown field type '%@'", fieldTypeString);
+        _fieldType = EMFieldTypeFromString(fieldTypeString);
         
         NSString *widgetTypeString = [[dict objectForKey:@"widget_type"] stringOrNil];
         
-        if ([widgetTypeString isEqual:@"text"])
-            _widgetType = EMFieldWidgetTypeText;
-        else if ([widgetTypeString isEqual:@"long"])
-            _widgetType = EMFieldWidgetTypeLong;
-        else if ([widgetTypeString isEqual:@"check_multiple"])
-            _widgetType = EMFieldWidgetTypeCheckMultiple;
-        else if ([widgetTypeString isEqual:@"radio"])
-            _widgetType = EMFieldWidgetTypeRadio;
-        else if ([widgetTypeString isEqual:@"select one"])
-            _widgetType = EMFieldWidgetTypeSelectOne;
-        else if ([widgetTypeString isEqual:@"select multiple"])
-            _widgetType = EMFieldWidgetTypeSelectMultiple;
-        else if ([widgetTypeString isEqual:@"date"])
-            _widgetType = EMFieldWidgetTypeDate;
-        else
-            NSLog(@"-[Field initWithDictionary]: encountered unknown widget type '%@'", widgetTypeString);
+        _widgetType = EMFieldWidgetTypeFromString(widgetTypeString);
         
         _options = [[dict objectForKey:@"options"] arrayOrNil];
         
